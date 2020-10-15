f8f6af4016d4c06d319bd9bd0aa21a0a65e7ce43
enoyao <enoyao@tencent.com>
2020-10-15 11:04:39 +0800

    更新demo
    更新demo

--- contributes/src/index.ts
+++ contributes/src/index.ts
@@ -1,150 +1,85 @@
-import { ExtensionsRegistry } from './vs/workbench/services/extensions/common/extensionsRegistry';
+import { ExtensionsRegistry, ExtensionMessageCollector, ExtensionPoint, IExtensionPoint, IExtensionPointUser } from './vs/workbench/services/extensions/common/extensionsRegistry';
+import { IExtensionManifest, ExtensionIdentifier, IExtensionDescription, } from './vs/platform/extensions/common/extensions';
+import { ActivationTimes, ExtensionPointContribution, IExtensionService, IExtensionsStatus, IMessage, IWillActivateEvent, IResponsiveStateChangeEvent, toExtension } from './vs/workbench/services/extensions/common/extensions';
 import { localize } from './vs/nls';
-console.log(1);
-console.log(ExtensionsRegistry);
+import { URI } from './vs/base/common/uri';
+// @ts-ignore
+import config from './config.json';
 
-export type JSONSchemaType = 'string' | 'number' | 'integer' | 'boolean' | 'null' | 'array' | 'object';
-
-export interface IJSONSchema {
-    id?: string;
-    $id?: string;
-    $schema?: string;
-    type?: JSONSchemaType | JSONSchemaType[];
-    title?: string;
-    default?: any;
-    definitions?: IJSONSchemaMap;
-    description?: string;
-    properties?: IJSONSchemaMap;
-    patternProperties?: IJSONSchemaMap;
-    additionalProperties?: boolean | IJSONSchema;
-    minProperties?: number;
-    maxProperties?: number;
-    dependencies?: IJSONSchemaMap | { [prop: string]: string[] };
-    items?: IJSONSchema | IJSONSchema[];
-    minItems?: number;
-    maxItems?: number;
-    uniqueItems?: boolean;
-    additionalItems?: boolean | IJSONSchema;
-    pattern?: string;
-    minLength?: number;
-    maxLength?: number;
-    minimum?: number;
-    maximum?: number;
-    exclusiveMinimum?: boolean | number;
-    exclusiveMaximum?: boolean | number;
-    multipleOf?: number;
-    required?: string[];
-    $ref?: string;
-    anyOf?: IJSONSchema[];
-    allOf?: IJSONSchema[];
-    oneOf?: IJSONSchema[];
-    not?: IJSONSchema;
-    enum?: any[];
-    format?: string;
-
-    // schema draft 06
-    const?: any;
-    contains?: IJSONSchema;
-    propertyNames?: IJSONSchema;
-
-    // schema draft 07
-    $comment?: string;
-    if?: IJSONSchema;
-    then?: IJSONSchema;
-    else?: IJSONSchema;
-
-    // VS Code extensions
-    defaultSnippets?: IJSONSchemaSnippet[];
-    errorMessage?: string;
-    patternErrorMessage?: string;
-    deprecationMessage?: string;
-    markdownDeprecationMessage?: string;
-    enumDescriptions?: string[];
-    markdownEnumDescriptions?: string[];
-    markdownDescription?: string;
-    doNotSuggest?: boolean;
-    suggestSortText?: string;
-    allowComments?: boolean;
-    allowTrailingCommas?: boolean;
+export interface IRawLanguageExtensionPoint {
+    command: string;
 }
 
-export interface IJSONSchemaMap {
-    [name: string]: IJSONSchema;
-}
-
-export interface IJSONSchemaSnippet {
-    label?: string;
-    description?: string;
-    body?: any; // a object that will be JSON stringified
-    bodyText?: string; // an already stringified JSON object that can contain new lines (\n) and tabs (\t)
-}
-
-
-const commandType: IJSONSchema = {
-    type: 'object',
-    required: ['command', 'title'],
-    properties: {
-        command: {
-            description: localize('vscode.extension.contributes.commandType.command', 'Identifier of the command to execute'),
-            type: 'string'
-        },
-        title: {
-            description: localize('vscode.extension.contributes.commandType.title', 'Title by which the command is represented in the UI'),
-            type: 'string'
-        },
-        category: {
-            description: localize('vscode.extension.contributes.commandType.category', '(Optional) Category string by the command is grouped in the UI'),
-            type: 'string'
-        },
-        enablement: {
-            description: localize('vscode.extension.contributes.commandType.precondition', '(Optional) Condition which must be true to enable the command'),
-            type: 'string'
-        },
-        icon: {
-            description: localize('vscode.extension.contributes.commandType.icon', '(Optional) Icon which is used to represent the command in the UI. Either a file path, an object with file paths for dark and light themes, or a theme icon references, like `$(zap)`'),
-            anyOf: [{
-                type: 'string'
-            },
-            {
-                type: 'object',
-                properties: {
-                    light: {
-                        description: localize('vscode.extension.contributes.commandType.icon.light', 'Icon path when a light theme is used'),
-                        type: 'string'
-                    },
-                    dark: {
-                        description: localize('vscode.extension.contributes.commandType.icon.dark', 'Icon path when a dark theme is used'),
-                        type: 'string'
-                    }
-                }
-            }]
+ExtensionsRegistry.registerExtensionPoint<IRawLanguageExtensionPoint[]>({
+    extensionPoint: 'toolbar',
+    jsonSchema: {}
+}).setHandler((extensions: readonly IExtensionPointUser<IRawLanguageExtensionPoint[]>[]) => {
+    for (let i = 0, len = extensions.length; i < len; i++) {
+        let extension = extensions[i];
+        console.log(extension);
+        for (let j = 0, lenJ = extension.value.length; j < lenJ; j++) {
+            let ext = extension.value[j];
+            console.log(ext.command);
         }
     }
-};
+});
 
-ExtensionsRegistry.registerExtensionPoint({
-    extensionPoint: 'toolbar',
-    jsonSchema: {
-        description: localize('vscode.extension.contributes.commands', "Contributes commands to the command palette."),
-        oneOf: [
-            commandType,
-            {
-                type: 'array',
-                items: commandType
+const hasOwnProperty = Object.hasOwnProperty;
+function _doHandleExtensionPoints(affectedExtensions: IExtensionDescription[]): void {
+    const affectedExtensionPoints: { [extPointName: string]: boolean; } = Object.create(null);
+    for (let extensionDescription of affectedExtensions) {
+        if (extensionDescription.contributes) {
+            for (let extPointName in extensionDescription.contributes) {
+                if (hasOwnProperty.call(extensionDescription.contributes, extPointName)) {
+                    // 收集能启动的配置项
+                    affectedExtensionPoints[extPointName] = true;
+                }
             }
-        ]
+        }
     }
-}).setHandler((extensions) => {
-    console.log(234);
-    console.log(extensions);
-});;
 
-const extensionPoints = ExtensionsRegistry.getExtensionPoints();
+    // const messageHandler = (msg: IMessage) => this._handleExtensionPointMessage(msg);
+    // 过滤可启用的配置项
+    // const availableExtensions = this._registry.getAllExtensionDescriptions();
+    const messageHandler = () => { console.log(1) }
+    const availableExtensions = affectedExtensions;
+    const extensionPoints = ExtensionsRegistry.getExtensionPoints();
+    for (const extensionPoint of extensionPoints) {
+        // 将可用的配置项进行注册
+        if (affectedExtensionPoints[extensionPoint.name]) {
+            _handleExtensionPoint(extensionPoint, availableExtensions, messageHandler);
+        }
+    }
+}
+
+function _handleExtensionPoint<T>(extensionPoint: ExtensionPoint<T>, availableExtensions: IExtensionDescription[], messageHandler: (msg: IMessage) => void): void {
+    const users: IExtensionPointUser<T>[] = [];
+    for (const desc of availableExtensions) {
+        if (desc.contributes && hasOwnProperty.call(desc.contributes, extensionPoint.name)) {
+            users.push({
+                description: desc,
+                value: desc.contributes[extensionPoint.name as keyof typeof desc.contributes],
+                collector: new ExtensionMessageCollector(messageHandler, desc, extensionPoint.name)
+            });
+        }
+    }
+    extensionPoint.acceptUsers(users);
+}
+
+interface IStaticExtension {
+    packageJSON: IExtensionManifest;
+    extensionLocation: URI;
+}
+
+// 读插件配置
 // @ts-ignore
-console.log(extensionPoints[0]._handler())
-// const affectedExtensionPoints: { [extPointName: string]: boolean; } = Object.create(null);
-// for (const extensionPoint of extensionPoints) {
-//     if (affectedExtensionPoints[extensionPoint.name]) {
-//         AbstractExtensionService._handleExtensionPoint(extensionPoint, availableExtensions, messageHandler);
-//     }
-// }
+const staticExtensions: readonly IStaticExtension[] = config.staticExtensions;
+
+const descriptions = staticExtensions.map(data => <IExtensionDescription>{
+    identifier: new ExtensionIdentifier(`${data.packageJSON.publisher}.${data.packageJSON.name}`),
+    extensionLocation: data.extensionLocation,
+    ...data.packageJSON,
+});
+
+// 处理所有插件配置项
+_doHandleExtensionPoints(descriptions);
